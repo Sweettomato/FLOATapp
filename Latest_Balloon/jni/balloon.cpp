0c472b85fc668a91569232d9b185de72cf0d5d78
auwong <auwong@bu.edu>
2013-12-05 21:42:55 -0500

    Turned updateTime into a function and other stuff

--- Latest_Balloon/jni/balloon.cpp
+++ Latest_Balloon/jni/balloon.cpp
@@ -24,6 +24,7 @@ class engine
 {
 	float Vx;
 	float Vy;
+	float Vz;
 	bool _hasFocus;
 
 	//Won't be used but implemented here anyways
@@ -41,6 +42,7 @@ class engine
 
 	void showUI();
 	void moveBalloon(); //Change parameters
+	void updateTime();
 
 public:
 	static void handleCmd(struct android_app* app, int32_t cmd);
@@ -87,75 +89,14 @@ void engine::setState(android_app* state)	//Should be done
 
 int engine::initDisplay()	//Should be done
 {
-	// initialize OpenGL ES and EGL
-
-	    /*
-	     * Here specify the attributes of the desired configuration.
-	     * Below, we select an EGLConfig with at least 8 bits per color
-	     * component compatible with on-screen windows
-	     */
-	   /* const EGLint attribs[] = {
-	            EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
-	            EGL_BLUE_SIZE, 8,
-	            EGL_GREEN_SIZE, 8,
-	            EGL_RED_SIZE, 8,
-	            EGL_NONE
-	    };
-	    EGLint w, h, dummy, format;
-	    EGLint numConfigs;
-	    EGLConfig config;
-	    EGLSurface surface;
-	    EGLContext context;
-
-	    EGLDisplay display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-
-	    eglInitialize(display, 0, 0);
-
-	     Here, the application chooses the configuration it desires. In this
-	     * sample, we have a very simplified selection process, where we pick
-	     * the first EGLConfig that matches our criteria
-	    eglChooseConfig(display, attribs, &config, 1, &numConfigs);
-
-	     EGL_NATIVE_VISUAL_ID is an attribute of the EGLConfig that is
-	     * guaranteed to be accepted by ANativeWindow_setBuffersGeometry().
-	     * As soon as we picked a EGLConfig, we can safely reconfigure the
-	     * ANativeWindow buffers to match, using EGL_NATIVE_VISUAL_ID.
-	    eglGetConfigAttrib(display, config, EGL_NATIVE_VISUAL_ID, &format);
-
-	    ANativeWindow_setBuffersGeometry(engine->app->window, 0, 0, format);
-
-	    surface = eglCreateWindowSurface(display, config, engine->app->window, NULL);
-	    context = eglCreateContext(display, config, NULL, NULL);
-
-	    if (eglMakeCurrent(display, surface, surface, context) == EGL_FALSE) {
-	        LOGW("Unable to eglMakeCurrent");
-	        return -1;
-	    }
-
-	    eglQuerySurface(display, surface, EGL_WIDTH, &w);
-	    eglQuerySurface(display, surface, EGL_HEIGHT, &h);
-
-	    engine->display = display;
-	    engine->context = context;
-	    engine->surface = surface;
-	    engine->width = w;
-	    engine->height = h;
-	    engine->state.angle = 0;
-
-	    // Initialize GL state.
-	    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_FASTEST);
-	    glEnable(GL_CULL_FACE);
-	    glShadeModel(GL_SMOOTH);
-	    glDisable(GL_DEPTH_TEST);*/
 	showUI();
 	return 0;
 }
 
 void engine::drawFrame() 	//Do this
 {
-	float Vx;
-	float Vy;
 	//moveBalloon();
+	updateTime();
 }
 void engine::termDisplay()
 {
@@ -275,8 +216,22 @@ void engine::moveBalloon() //Don't use this yet
 
     //Default class retrieval
     jclass clazz = jni->GetObjectClass( _app->activity->clazz );
-    jmethodID methodID = jni->GetMethodID(clazz, "moveBalloon", "(FF)V" );
-    jni->CallVoidMethod( _app->activity->clazz, methodID, Vx, Vy);
+    jmethodID methodID = jni->GetMethodID(clazz, "moveBalloon", "()V" );
+    jni->CallVoidMethod( _app->activity->clazz, methodID);
+
+    _app->activity->vm->DetachCurrentThread();
+    return;
+}
+
+void engine::updateTime()
+{
+    JNIEnv *jni;
+    _app->activity->vm->AttachCurrentThread(&jni, NULL);
+
+    //Default class retrieval
+    jclass clazz = jni->GetObjectClass( _app->activity->clazz );
+    jmethodID methodID = jni->GetMethodID(clazz, "updateTime", "()V" );
+    jni->CallVoidMethod( _app->activity->clazz, methodID);
 
     _app->activity->vm->DetachCurrentThread();
     return;
@@ -302,6 +257,12 @@ void engine::processSensors(int32_t id) //Should be done
             while (ASensorEventQueue_getEvents(_sensorEventQueue,
                     &event, 1) > 0)
             {
+                Vx = event.acceleration.x;
+                Vy = event.acceleration.y;
+                Vz = event.acceleration.z;
+
+            	LOGI("accelerometer: x=%f y=%f z=%f",
+                        Vx, Vy, Vz);
             }
         }
     }
--- Latest_Balloon/src/com/ec327/ballon/GameScreen.java
+++ Latest_Balloon/src/com/ec327/ballon/GameScreen.java
@@ -18,15 +18,14 @@ public class GameScreen extends NativeActivity {
 	private int seconds = 0, minutes = 0;
 	private Handler handler = new Handler();
 	private TextView textViewTimer;
-	private Runnable updateTime;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.game_screen);
+		//setContentView(R.layout.game_screen);
 		balloon = (ImageView) findViewById(R.id.balloon);
 		textViewTimer = (TextView) findViewById(R.id.textViewTimer);
-		updateTime = new Runnable() {
+/*		updateTime = new Runnable() {
 			public void run() {
 				seconds++;
 				if (seconds >= 60) {
@@ -45,9 +44,9 @@ public class GameScreen extends NativeActivity {
 				textViewTimer.setText(minuteString + ":" + secondString);
 				handler.postDelayed(this, 1000);
 			}
-		};
+		};*/
 
-		handler.postDelayed(updateTime, 0);
+		//handler.postDelayed(updateTime, 0);
 	}
 
 	static {
@@ -57,6 +56,7 @@ public class GameScreen extends NativeActivity {
 	GameScreen _activity;
 	PopupWindow _popupWindow;
 	TextView _label;
+	ImageView _balloon;
 
 	public void showUI() {
 		if (_popupWindow != null)
@@ -72,23 +72,51 @@ public class GameScreen extends NativeActivity {
 				View popupView = layoutInflater.inflate(R.layout.game_screen,
 						null);
 				_popupWindow = new PopupWindow(popupView,
-						LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+						LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
 
 				LinearLayout mainLayout = new LinearLayout(_activity);
 				MarginLayoutParams params = new MarginLayoutParams(
-						LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+						LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
 				params.setMargins(0, 0, 0, 0);
 				_activity.setContentView(mainLayout, params);
 
 				// Show our UI over NativeActivity window
 				_popupWindow.showAtLocation(mainLayout, Gravity.TOP
-						| Gravity.LEFT, 10, 10);
+						| Gravity.LEFT, 0, 0);
 				_popupWindow.update();
-
+				
 				_label = (TextView) popupView.findViewById(R.id.textViewTimer);
-
+				_balloon = (ImageView) popupView.findViewById(R.id.balloon);
 			}
 		});
+	}
+	
+	public void updateTime() {
+		if (_label !=null)
+			return;
+		_activity = this;
+		this.runOnUiThread(new Runnable() {
+			public void run() {
+				seconds++;
+				if (seconds >= 60) {
+					minutes++;
+					seconds = 0;
+				}
+
+				String secondString = Integer.toString(seconds);
+				if (seconds < 10) {
+					secondString = "0" + secondString;
+				}
+				String minuteString = Integer.toString(minutes);
+				if (minutes < 10) {
+					minuteString = "0" + minuteString;
+				}
+				//_label.setText(minuteString + ":" + secondString);
+				_label.setText(String.format("%s: %s", minuteString, secondString));
+				handler.postDelayed(this, 1000);
+			}
+		});
+		
 	}
 
 }
